bae7848ace44e56342ee0e7177be17414a4b18ed
dashnonews-oss <dashnonews@gmail.com>
2025-08-20 09:04:45 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~N7cTTRCD#kcU-7x2SCnfEAegvRgKdXBd_mvtwSjZqSgMC1lDaktg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "18293511531", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "!", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
